b8bb61d65c2d95e74c423bd81ac7eafcd95fc08a
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-11-07 23:02:52 -0300

    wip-Feat: implementada uma função para permitir apenas uma checkbox marcada por vez, e também baixado o pacote PropTypes

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,23 +1,53 @@
+import React, { useState } from 'react'
 import '../css/Header.css';
 
 function Header() {
+  const initialChecked = {
+    alphabetical: false,
+    date: false,
+    status: false,
+  };
+  const [statusCheckboxes, setStatusCheckboxes] = useState(initialChecked);
+
+  function handleChange({ target : { name, checked } } ) {
+    setStatusCheckboxes({ ...initialChecked, [name]: checked });
+  }
+
   return(
     <header id='main-header'>
       <form id='sort-form'>
         <span>Ordenar por:</span>
 
         <label htmlFor='alphabetical-order-checkbox'>
-          <input type='checkbox' id='alphabetical-order-checkbox' />
+          <input
+            type='checkbox'
+            id='alphabetical-order-checkbox'
+            name='alphabetical'
+            checked={ statusCheckboxes.alphabetical}
+            onChange={ (event) => handleChange(event) }
+          />
           Ordem Alfabética
         </label>
 
         <label htmlFor='date-checkbox'>
-          <input type='checkbox' id='date-checkbox' />
+          <input
+            type='checkbox'
+            id='date-checkbox'
+            name='date'
+            checked={ statusCheckboxes.date}
+            onChange={ (event) => handleChange(event) }
+          />
           Data
         </label>
 
         <label htmlFor='status-checkbox'>
-          <input type='checkbox' id='status-checkbox' />
+          <input
+            type='checkbox'
+            id='status-checkbox'
+            name='status'
+            checked={ statusCheckboxes.status}
+            onChange={ (event) => handleChange(event) }
+          />
           Status
         </label>
       </form>
